0c11ad1150c7b878a87312aa855ea5ba7083d883
tetsu-nn <182674201+tetsu-nn@users.noreply.github.com>
2024-11-24 17:06:38 +0000

    image

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,14 +7,22 @@ class BooksController < ApplicationController
     @book=Book.new(book_params)
     @book.user_id= current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to books_path()
   end
 
   def index
+    @books=Book.all
   end
 
   def show
+    @book=Book.find(params[:id])
   end
+
+  def destroy
+     book= Book.find(params[:id])
+     book.destroy
+     redirect_to books_path
+  end  
 
   private
 
